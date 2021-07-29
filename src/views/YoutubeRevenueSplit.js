2c5953f6362ce99581adc4c6d8aadb1365cc8137
Tyler Barron <tylerb@productcreationstudio.com>
2021-07-28 19:01:27 -0600

    v0.0.1: pre-demo readiness

--- src/views/YoutubeRevenueSplit.js
+++ src/views/YoutubeRevenueSplit.js
@@ -19,32 +19,38 @@ export default function YoutubeRevenueSplit(_user) {
   const [error, setError] = useState(null);
   const [authenticated, setAuthenticated] = useState(false);
   const [user, setuser] = useState(null);
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
 
 
-
-  const _handleSignInClick = () => {
+  const _handleSignInGoogle = () => {
     // Authenticate using via passport api in the backend
     // Open Twitter login page
     // Upon successful login, a cookie session will be stored in the client
     window.open("http://localhost:4000/auth/google", "_self");
   }
 
-
   useEffect(() => {
     // Fetch does not send cookies. So you should add credentials: 'include'
-    fetch("http://localhost:4000/", {
+    _logOn();
+  }, []);
+
+  function _logOn() {
+    Axios({
       method: "GET",
-      credentials: "include",
+      withCredentials: true,
+      url: "http://localhost:4000/",
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
         "Access-Control-Allow-Credentials": true
       }
+    }).then(response => {
+      if (response.status === 200) {
+        return response.data;
+      }
+      throw new Error("failed to authenticate user");
     })
-      .then(response => {
-        if (response.status === 200) return response.json();
-        throw new Error("failed to authenticate user");
-      })
       .then(responseJson => {
         setAuthenticated(true);
         setuser(responseJson.user);
@@ -54,25 +60,138 @@ export default function YoutubeRevenueSplit(_user) {
         setAuthenticated(false);
         setError("Failed to authenticate user");
       });
-  });
+  }
 
-  const _handleNotAuthenticated = () => {
-    setAuthenticated(false);
+  function _handleRegister() {
+    Axios({
+      method: "POST",
+      data: {
+        username: registerUsername,
+        password: registerPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/auth/register",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        "Access-Control-Allow-Credentials": true
+      }
+    }).then((res) => console.log(res));
   };
 
+  function _handleSignInLocal() {
+    Axios({
+      method: "POST",
+      data: {
+        username: registerUsername,
+        password: registerPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/auth/login",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        "Access-Control-Allow-Credentials": true
+      }
+    }).then((res) => {
+      console.log(res)
+      _logOn();
+    });
+  };
+
+  function _handleSignOut() {
+    Axios({
+      method: "GET",
+      data: {},
+      withCredentials: true,
+      url: "http://localhost:4000/auth/logoff",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        "Access-Control-Allow-Credentials": true
+      }
+    }).then((res) => {
+      console.log("res")
+      _logOn();
+    }).catch((err) => {
+      _logOn();
+    });
+  };
+
+  
+
   return (
-    <div>
-      <a onClick={_handleSignInClick}>Login</a>
-      <div>
-        {!authenticated ? (
-          <h1>Welcome!</h1>
-        ) : (
-          <div>
-            <h1>You have logged in successfully!</h1>
-            <h2>Welcome {name}!</h2>
-          </div>
-        )}
-      </div>
+
+    <div className="bg-background bg-fixed">
+      <Navbar />
+      <main>
+
+        <Article
+          title="Authentication Example"
+          subtitle=""
+        >
+
+          <Subarticle
+            subtitle=""
+          >
+            <div>
+              {!authenticated ? (
+                <h1>Welcome!</h1>
+              ) : (
+                <div>
+                  <h1>You have logged in successfully!</h1>
+                  <h2>Welcome {name}!</h2>
+                </div>
+              )}
+            </div>
+
+            <h3 className="text-xl text-center pb-4">Log On</h3>
+
+            <div className="w-2/4 mx-auto h-16 pb-4">
+              <Input
+                id={"Entry Name"}
+                label={"Entry Name"}
+                handleChange={(e) => setRegisterUsername(e)}
+              />
+            </div>
+
+            <div className="w-2/4 mx-auto h-16 pb-4">
+              <Input
+                id={"Password"}
+                label={"Password"}
+                isPassword={true}
+                handleChange={(e) => setRegisterPassword(e)}
+
+              />
+            </div>
+
+            <div className="w-1/4 mx-auto h-12 mb-4 bg-white rounded cursor-pointer" onClick={_handleSignInLocal}>
+              <div className="w-full h-full text-center text-lg place-self-center pt-2">Log On</div>
+            </div>
+
+            <div className="w-1/4 mx-auto h-12 mb-4 bg-white rounded cursor-pointer" onClick={_handleSignOut}>
+              <div className="w-full h-full text-center text-lg place-self-center pt-2">Log Off</div>
+            </div>
+
+            <div className="w-1/4 mx-auto h-12 mb-4 bg-red-light rounded cursor-pointer" onClick={_handleRegister}>
+              <div className="w-full h-full text-center text-lg place-self-center pt-2">Register</div>
+            </div>
+
+            <br></br>
+
+
+            <hr></hr>
+            <br></br>
+
+            <div className="w-2/4 mx-auto h-12 mb-4 bg-red-light rounded cursor-pointer" onClick={_handleSignInGoogle}>
+              <div className="w-full h-full text-center text-lg place-self-center pt-2">Log On with Google</div>
+            </div>
+
+          </Subarticle>
+        </Article>
+
+      </main>
+      <Footer />
     </div>
   );
 
