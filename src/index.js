7dcd337285f0dd74b6eb1a4cc104ad667a964f47
Tyler Barron <trbbarron@gmail.com>
2024-01-30 12:12:41 -0700

    Added posthog

--- src/index.js
+++ src/index.js
@@ -1,85 +1,58 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './index.css';
 
+// Import your views
 import Profile from "./views/Profile";
 import Home from "./views/Home";
 import TheRiddler from "./views/TheRiddler.js";
 import CamelUpCup from "./views/CamelUpCup";
-import GenerativeArt from "./views/GenerativeArt"
-import BoulderingTracker from "./views/BoulderingTracker"
-import Set from "./views/Set"
-import SSBM from "./views/SSBM"
-import SwiftneyGame from "./views/SwiftneyGame.js"
-import ChessOpenings from "./views/ChessOpenings.js"
-import RiddlerWarfare from "./views/RiddlerWarfare"
-import LudwigChess from "./views/LudwigChess.js"
+import GenerativeArt from "./views/GenerativeArt";
+import BoulderingTracker from "./views/BoulderingTracker";
+import Set from "./views/Set";
+import SSBM from "./views/SSBM";
+import SwiftneyGame from "./views/SwiftneyGame.js";
+import ChessOpenings from "./views/ChessOpenings.js";
+import RiddlerWarfare from "./views/RiddlerWarfare";
+import LudwigChess from "./views/LudwigChess.js";
 import LudwigChessHostLanding from "./views/LudwigChessHostLanding";
 import LudwigChessInviteLanding from "./views/LudwigChessInviteLanding";
 import LudwigChessAudienceChess from "./views/LudwigChessAudienceLanding";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/profile">
-        <Profile />
-      </Route>
+// Import PostHog
+import { PostHogProvider } from 'posthog-js/react';
 
-      <Route path="/TheRiddler">
-        <TheRiddler />
-      </Route>
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+};
 
-      <Route path="/CamelUpCup">
-        <CamelUpCup />
-      </Route>
-
-      <Route path="/GenerativeArt">
-        <GenerativeArt />
-      </Route>
-
-      <Route path="/BoulderingTracker">
-        <BoulderingTracker />
-      </Route>
-
-      <Route path="/Set">
-        <Set />
-      </Route>
-
-      <Route path="/SSBM">
-        <SSBM />
-      </Route>
-
-      <Route path="/SwiftneyGame">
-        <SwiftneyGame />
-      </Route>
-
-      <Route path="/ChessOpenings">
-        <ChessOpenings />
-      </Route>
-
-      <Route path="/RiddlerWarfare">
-        <RiddlerWarfare />
-      </Route>
-
-      <Route path="/LudwigChess/:gameID/:name">
-        <LudwigChess />
-      </Route>
-
-      <Route path="/LudwigChess/:gameID/">
-        <LudwigChessInviteLanding />
-      </Route>
-
-      <Route path="/LudwigChess/">
-        <LudwigChessHostLanding />
-      </Route>
-      <Route path="/LudwigChessLanding/">
-        <LudwigChessAudienceChess />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <PostHogProvider 
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <BrowserRouter>
+        <Switch>
+          <Route path="/profile" component={Profile} />
+          <Route path="/TheRiddler" component={TheRiddler} />
+          <Route path="/CamelUpCup" component={CamelUpCup} />
+          <Route path="/GenerativeArt" component={GenerativeArt} />
+          <Route path="/BoulderingTracker" component={BoulderingTracker} />
+          <Route path="/Set" component={Set} />
+          <Route path="/SSBM" component={SSBM} />
+          <Route path="/SwiftneyGame" component={SwiftneyGame} />
+          <Route path="/ChessOpenings" component={ChessOpenings} />
+          <Route path="/RiddlerWarfare" component={RiddlerWarfare} />
+          <Route path="/LudwigChess/:gameID/:name" component={LudwigChess} />
+          <Route path="/LudwigChess/:gameID/" component={LudwigChessInviteLanding} />
+          <Route path="/LudwigChess/" component={LudwigChessHostLanding} />
+          <Route path="/LudwigChessLanding/" component={LudwigChessAudienceChess} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </PostHogProvider>
+  </React.StrictMode>
 );
