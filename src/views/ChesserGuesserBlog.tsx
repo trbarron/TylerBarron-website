1085d1257e7d0abf4d6ce9dab96f1f8ec334e7d2
Tyler Barron <trbbarron@gmail.com>
2024-04-16 08:27:00 -0600

    chesser guesser blog

--- src/views/ChesserGuesserBlog.tsx
+++ src/views/ChesserGuesserBlog.tsx
@@ -5,7 +5,7 @@ import Article from '../components/Article';
 import Photo from '../components/Photo';
 
 import chessBoard from '../assets/img/ChesserGuesser/screenshot.png';
-import analyticsGraph from '../assets/img/Chess/analyticsGraph.jpg';
+import fen from '../assets/img/ChesserGuesser/fen.png';
 import communityInteraction from '../assets/img/Chess/communityInteraction.jpg';
 
 export default function ChessBlog() {
@@ -28,7 +28,6 @@ export default function ChessBlog() {
               caption="A sample chess position from Chesser Guesser"
             />
 
-            <p>The game integrates with the <a href='https://lichess.org/@/lichess/blog/thousands-of-stockfish-analysers/WN-gLzAA'>Lichess Cloud Analysis</a> to fetch position evaluations at scale, giving access to all the positions and their evaluations without me having to do any work. Having this resource made the tough part of this project incredibly easy.</p>
           </Subarticle>
 
       </Article>
@@ -37,16 +36,34 @@ export default function ChessBlog() {
           subtitle=""
         >
           <Subarticle
-            subtitle="The How"
+            subtitle="The Analysis"
           >
+            <p>The game integrates with the <a href='https://lichess.org/@/lichess/blog/thousands-of-stockfish-analysers/WN-gLzAA'>Lichess Cloud Analysis</a> to fetch position evaluations at scale, giving access to all the positions and their evaluations without me having to do any work. Having this resource made the tough part of this project incredibly easy.</p>
+            
+            <Photo
+              src={fen}
+              alt="FEN Notation Example"
+              caption="How FEN Notation is used to represent a chess position"
+            />
+
             <p>Chesser Guesser uses Python connected to several Amazon DynamoDB instances for data storage. Lichess gives us a huge number of analyzed positions – we get to parse those down and only insert the interesting ones for our game. The criteria used was: </p>
-            <p className='pl-8'>- The evaluation is not between -50 and 50 centipawns (in either direction to avoid ties)</p>
-            <p className='pl-8'>- The evaluation is not above 400 centipawn (in either direction, would make the UI annoying)</p>
+            <p className='pl-8'>- The evaluation is not above 400 centipawns (a centipawn is a unit of advantage, with 100 ~= 1 pawn's advantage) in either direction or between -50 and 50 centipawns</p>
             <p className='pl-8'>- The same number of entries must be given for both the black and white side</p>
             <p className='pl-8'>- There are less than 5 pawns on any rank, to remove most analysis being on openings</p>
-            <p>A total of 400 evaluations were added, although thousands meet the criteria</p>
-            <hr></hr>
-            <p>Then we have another application database that is also a DynamoDB that stores the daily user's scores. These will have the date, their total score and their name. The score is just the sums of the differences between their guesses and the computer's evaluation from all five rounds, which is what can land you on the leaderboards.</p>
+            <p>A total of 400 evaluations were added, although thousands meet the criteria and there are over a million with saved analysis</p>
+            
+
+          </Subarticle>
+          <Subarticle
+            subtitle="The User Generated Content"
+          >
+            <p>Then we have another application database that is also a DynamoDB that stores the daily user's scores. These will have the date, their total score and their name. The score is just the sums of the differences between their guesses and the computer's evaluation from all five rounds, which is what can land you on the leaderboards. Even with the users we were able to stay within the free tier</p>
+          </Subarticle>
+          <Subarticle
+            subtitle="The Rest of the UI"
+          >
+            <p>For the chess board I used the open source <a href='https://github.com/lichess-org/chessground/tree/master'>Chessground</a>. I've used it before and gotta say, its the best. Again, thank you to Lichess for providing these resources! </p>
+            <p>Sliders and such were able to be reused from the <a href='https://www.tylerbarron.com/ChessOpenings'>Chess Openings Practice</a> project. I made a few improvements to help with it on mobile (75+% of users are mobile users) which is always great.</p>
           </Subarticle>
         </Article>
         <Article
